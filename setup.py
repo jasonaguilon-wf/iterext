c4fe1a215f0b6bf6bbde68663898bbef0c34099d
Jason Aguilon <jason.aguilon@webfilings.com>
2013-10-08 15:26:59 -0400

    Update package setup version and description

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ def read(fname):
 
 setup(
     name="iterext",
-    version="0.0.1",
+    version="0.0.2",
     url='http://github.com/aguil/iterext',
     author="Jason Aguilon",
     author_email="jaguilon@gmail.com",
-    description='iterext: Python itertools recipes.',
+    description='iterext: Python itertools recipes and extras.',
     long_description=read('README.rst'),
     packages=find_packages(),
     classifiers=['Development Status :: 1 - Planning',
